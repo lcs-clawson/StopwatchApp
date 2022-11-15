0c89ada645fb1c886257102e3a7e6c33fafc29d3
Cooper Lawson <CSLawson@lcs.on.ca>
2022-11-15 15:06:33 -0500

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Cooper Lawson on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
